baf63a319469c20e7228f25de63712b8d40dbd55
RicargoGGx <ricardogallegos@example.com>
2026-05-12 17:36:52 -0600

    ✨ feat: add 3D flip animation to Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,16 @@
-export default function Card({ card, isFlipped, isMatched, onClick }) {
+export default function Card({ card, isFlipped, isMatched, isDisabled, onClick }) {
   const flipped = isFlipped || isMatched;
 
   return (
     <div
-      className="card-scene"
-      onClick={onClick}
+      className={`card-scene${isDisabled && !flipped ? ' disabled' : ''}`}
+      onClick={!isDisabled ? onClick : undefined}
       role="button"
     >
-      <div className="card-inner">
+      <div className={`card-inner${flipped ? ' flipped' : ''}`}>
+        {/* Back face */}
         <div className="card-face card-back">?</div>
+        {/* Front face */}
         <div className="card-face card-front">
           <img src={card.img} alt={card.label} draggable="false" />
         </div>
